cb546f82d58934dcc4c41888535f7ee90266816e
Kacper <kacper@kacperadler.info>
2025-08-07 19:26:52 +0200

    Fixes (#6)

--- lrt_wallbox/__init__.py
+++ lrt_wallbox/__init__.py
@@ -1,4 +1,5 @@
 from .wallbox import WallboxClient
 from . import msg_types
+from .exceptions import WallboxError
 
-__all__ = ["WallboxClient", "msg_types", "exceptions"]
+__all__ = ["WallboxClient", "msg_types", "WallboxError"]
